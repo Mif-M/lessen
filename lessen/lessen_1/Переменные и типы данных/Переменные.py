271c1b277b4e3b0719516ab0b2415f360646dba5
Mihail Fedorov <misha.fedorov.2012@bk.ru>
2022-03-13 01:59:55 +0300

    Занятие 1

---
+++ lessen/lessen_1/Переменные и типы данных/Переменные.py
@@ -0,0 +1,22 @@
+name = "Name"  # string
+
+age = 22  # integer
+
+f = 5.6  # Float
+
+flag = True  # bool
+print(flag)
+# Значения переменных способны меняться
+flag = False  # bool
+print(flag)
+
+# age = 22 # int
+print(age)
+age = 22.0  # float
+print(age)
+age = "22"  # str
+print(age)
+
+# После выполнения кода ошибки не будет
+
+
