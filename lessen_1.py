07cee5dd7e2dd37ef1d567e12f3a4f8fa588c7a0
Mihail Fedorov <misha.fedorov.2012@bk.ru>
2022-03-12 23:00:28 +0300

    Занятие 1

--- lessen_1.py
+++ lessen_1.py
@@ -11,8 +11,6 @@ from datetime import datetime
 
 name = input("Как тебя зовут? ")
 print(f"Hello {name} !")
-
 age = int(input("Сколько тебе лет ?"))
-
 PythonAge = datetime.now().year - 1994
 print(f"А мне {PythonAge} )")
